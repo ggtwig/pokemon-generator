434df8a5205b5ff9a324b8f8f148ef2471f458c6
Jack Waters <61799247+ggtwig@users.noreply.github.com>
2022-10-11 00:15:13 +0100

    reworked shuckle.java test message

--- src/Creation/Pokemons/Shuckle.java
+++ src/Creation/Pokemons/Shuckle.java
@@ -8,7 +8,7 @@ import java.util.Random;
 public class Shuckle extends Pokemon{
     @Override
     public void test() {
-        System.out.println("##### Created!");
+        System.out.println("Shuckle Created!");
     }
 
     @Override
